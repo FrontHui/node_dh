d02034542386e0fcd707bb48d37dda59e862b7f4
杜辉 <EX_KJKFB_DUHUI@pingan.com.cn>
2018-11-08 19:13:44 +0800

    demo

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var fs = require('fs');
 var url = require('url');
 var server = http.createServer(function(sreq, sres){
     var url_parts = url.parse(sreq.url);
-    console.log(url_parts + '请求rul');
+    console.log(url_parts.path + '请求rul');
+    console.log(url_parts.pathname + '请求rul');
     var options = {
         host: 'tool.oschina.net',
         port: 80,
         path: url_parts.pathname,
-        headers: sreq.headers
+        // headers: sreq.headers
     };
 
     var creq = http.get(options, function (cres) {
@@ -16,6 +17,7 @@ var server = http.createServer(function(sreq, sres){
         cres.pipe(sres);
     });
     sreq.pipe(creq);
-}).listen(1337,"127.0.0.1", function () {
+})
+server.listen(1337,"127.0.0.1", function () {
 	console.log('服务端开始监听');
 });
